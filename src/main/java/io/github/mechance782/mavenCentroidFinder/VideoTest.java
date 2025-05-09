a9633b8a6c7efffe84bd664bb421f4bf01825efc
Kimberly Mageary <kimberlymageary@gmail.com>
2025-05-09 12:03:47 -0700

    Testing out the grabber!

--- src/main/java/io/github/mechance782/mavenCentroidFinder/VideoTest.java
+++ src/main/java/io/github/mechance782/mavenCentroidFinder/VideoTest.java
@@ -1,15 +1,30 @@
 package io.github.mechance782.mavenCentroidFinder;
-import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.FrameGrabber;
 
 
 public class VideoTest {
 
     public static void main(String[] args) {
-        FrameGrabber grabber = new FFmpegFrameGrabber(args[0]);
         
+        try(FrameGrabber grabber = new FFmpegFrameGrabber(args[0])){
+            grabber.start();
+            System.out.println("Grabber has been started!");
+            System.out.println("Video length in frames: " + grabber.getLengthInFrames());
+            System.out.println("Video format: " + grabber.getFormat());
+
+            Frame frame = grabber.grab();
+            if(frame != null){
+                System.out.println("Grabbed a frame");
+            } else{
+                System.out.println("Frame is null, failed to grab.");
+            }
+
+            grabber.stop();
+        }catch(Exception e){
+            System.out.println("Error: " + e);;
+        }
     }
     
 }
