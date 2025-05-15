6105266f948080d9a5ed3889e4b74d44d2b8147e
Kimberly Mageary <kimberlymageary@gmail.com>
2025-05-14 17:04:51 -0700

    Added main app file.

---
+++ src/main/java/io/github/mechance782/mavenCentroidFinder/CentroidTrackerApp.java
@@ -0,0 +1,11 @@
+package io.github.mechance782.mavenCentroidFinder;
+
+/**
+ * Parses command line arguments and launches the video processing tracker
+ * @param args
+ */
+public class CentroidTrackerApp {
+    public static void main(String[] args) {
+        
+    }
+}
