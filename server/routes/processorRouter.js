1e85e3ae81417649fe69f404ffbba4d5a932d49a
Madeleine Chance <mechance782@gmail.com>
2025-05-21 18:17:05 -0700

    Add router logic

--- server/routes/processorRouter.js
+++ server/routes/processorRouter.js
@@ -1,1 +1,13 @@
 // router
+import express from 'express';
+import controller from './../controllers/processorController.js';
+
+const router = express.Router();
+
+// mount controller functions to router
+router.get("/api/videos", controller.videos);
+router.get("/thumbnail/:filename", controller.thumbnail);
+router.get("/process/:jobId/status", controller.status);
+router.post("/process/:filename", controller.process);
+
+export default router;
