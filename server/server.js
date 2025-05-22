13a268225c729776160b8a8682eb3b9ceee84838
Kimberly Mageary <kimberlymageary@gmail.com>
2025-05-21 17:57:46 -0700

    Initializing main server file.

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import router from '?????';
+
+
+const app = express();
+const PORT = 3000;
+
+// Set the view to public folder
+app.use(express.static('public'));
+
+// Allows this server to accept requests from different domains (origins)
+app.use(cors());
+
+// This allows us to read in JSON payloads from the req body
+app.use(express.json());
+
+app.use("/", router);
+
+app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
