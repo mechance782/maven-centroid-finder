69a57c670cdb8535f433bfcb14491733beeb5c3e
Kimberly Mageary <kimberlymageary@gmail.com>
2025-05-27 16:03:34 -0700

    Implemented test headers for controllers.

--- server/tests/test.spec.js
+++ server/tests/test.spec.js
@@ -7,3 +7,45 @@ describe('add()', () => {
     expect(add(2, 3)).to.equal(5);
   });
 });
+
+/** --------------------- CONTROLLER TESTING --------------------- */
+describe('Controller Testing', ()=>{
+  /** -------- ALL VIDEOS -------- */
+  describe('allVideos', ()=>{
+    it('Should return list of videos & 200 status code', async()=>{
+
+    });
+    it('Should send 500 status if error occurs', async()=>{
+
+    });
+  });
+  /** -------- GET THUMBNAIL -------- */
+  describe('thumbnail', ()=>{
+    it('Should send file and 200 status code if successful', async()=>{
+
+    });
+    it('Should throw e if error occurs', async()=>{
+
+    });
+  });
+  /** -------- GET JOB STATUS -------- */
+  describe('jobStatus', ()=>{
+    it('Should send 200 and JSON with job info', async()=>{
+
+    });
+    //TODO: Figure out what else this method should do
+  });
+  /** --------  POST PROCESSING JOB -------- */
+  describe('processingJob', ()=>{
+    it('Should send 400 if missing/bad params', async()=>{
+
+    });
+    it('Should send 500 if error occurs', async()=>{
+
+    });
+    it('Should send 202 w/ job ID', async()=>{
+
+    });
+  });
+});
+
